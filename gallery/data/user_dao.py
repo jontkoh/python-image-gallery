a4822edffb072d6f9262baf005084895d34c59a4
Jonathan Koh <jontkoh@gmail.com>
2020-07-05 12:59:25 -0700

    moved around python files, added python modules with __init__, cleaned up code in app.py

---
+++ gallery/data/user_dao.py
@@ -0,0 +1,3 @@
+class UserDAO:
+  def get_users(self):
+    raise Exception("must be implemented")
--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -3,87 +3,28 @@ from flask import request
 from flask import render_template
 import psycopg2
 import json
-from . secrets import get_secret_image_gallery
-
-connection = None
-
-def get_secret():
-    jsonString = get_secret_image_gallery()
-    return json.loads(jsonString)
-
-def get_password(secret):
-    return secret['password']
-
-def get_host(secret):
-    return secret['host']
-
-def get_username(secret):
-    return secret['username']
-
-def get_dbname(secret):
-    return secret['database_name']
-
-def connect():
-    global connection
-    secret = get_secret()
-    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
-    cursor = connection.cursor()
-
-def execute(query):
-    global connection
-    cursor = connection.cursor()
-    cursor.execute(query)
-    cursor.close()
-    connection.close()
-
-#function for listing users
-def listUsers():
-    cursor = connection.cursor() 
-    cursor.execute('SELECT username, full_name FROM users;')
-    username = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return username
-
-#function for deleting users
-def deleteUser(user):
-    global connection
-    cursor = connection.cursor()
-    pgDelete = "DELETE from users where username = '" + user + "'"
-    cursor.execute(pgDelete)
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-#function for editing users
-def editUsers(user, password, fullName):
-    global connection
-    cursor = connection.cursor()
-    pgUpdate = """Update users set password = %s where username = %s"""
-    cursor.execute(pgUpdate, (password, user))
-
-    pgUpdate = """Update users set full_name = %s where username = %s"""
-    cursor.execute(pgUpdate, (fullName, user))
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-#function for creating users
-def createUser(user, password, fullName):
-    global connection
-    cursor = connection.cursor()
-    pgInsert = """INSERT INTO users (username, password, full_name) values (%s, %s, %s)"""
-    record = (user, password, fullName)
-    cursor.execute(pgInsert, record)
-    connection.commit()
-    cursor.close()
-    connection.close()
+from ..aws.secrets import get_secret_image_gallery
+from ..data.db import *
+from ..data.user import User
+from ..data.postgres_user_dao import PostgresUserDAO
 
 app = Flask(__name__)
+
+connect()
+
+def get_user_dao():
+    return PostgresUserDAO()
 
 @app.route('/')
 def hello_world():
     return 'please go to /admin'
+
+@app.route('/users')
+def users():
+    result = ""
+    for user in get_user_dao().get_users():
+        result += str(user)
+    return result
 
 @app.route('/admin')
 def hello_admin():
