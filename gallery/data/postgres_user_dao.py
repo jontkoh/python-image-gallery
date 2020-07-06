03ef7f00dddd91a9c27784e2801dff1448436acf
Jonathan Koh <jontkoh@gmail.com>
2020-07-05 17:12:41 -0700

    debugging login route

--- gallery/data/postgres_user_dao.py
+++ gallery/data/postgres_user_dao.py
@@ -14,7 +14,7 @@ class PostgresUserDAO(UserDAO):
     return result
 
   def get_user_by_username(self, username):
-    cursor = db.execute("select username, password, full_name from users where username = %s", (username, ))
+    cursor = db.execute("select username, password, full_name from users where username = " + username)
     row = cursor.fetchone()
     if row is None:
       return None
